eb9a9541127bbd8ea2ea51fe90ed46b1a1692fec
kj455 <kaji.ibuki45@gmail.com>
2022-06-05 16:07:50 +0900

    fix: activate command

--- activate.sh
+++ activate.sh
@@ -1,6 +1,9 @@
 BLUE_UTIL_PATH=`which blueutil`
 
 echo "$BLUE_UTIL_PATH -p 0" > ~/.sleep
+chmod 777 ~/.sleep
+
 echo "$BLUE_UTIL_PATH -p 1" > ~/.wakeup
+chmod 777 ~/.wakeup
 
 brew services start sleepwatcher
